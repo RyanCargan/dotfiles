4b956d108e7f1e28d052fdee26846a60e006c1b3
Ryan <ryancargan@gmail.com>
2026-08-05 12:28:45 +0530

    Tweak scrollbar

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,18 +18,15 @@ config.color_scheme = 'AdventureTime'
 config.window_background_opacity = 0.85   -- <-- add this line
 
 -- Enable auto-refresh
-automatically_reload_config = true
+config.automatically_reload_config = true
 
 -- Scrollbar
 config.enable_scroll_bar = true
--- Hide the scrollbar when there is no scrollback or alternate screen is active
+-- Hide the scrollbar when the alternate screen is active (e.g., vim, less)
 wezterm.on("update-status", function(window, pane)
-local overrides = window:get_config_overrides() or {}
-local dimensions = pane:get_dimensions()
-
-overrides.enable_scroll_bar = dimensions.scrollback_rows > dimensions.viewport_rows and not pane:is_alt_screen_active()
-
-window:set_config_overrides(overrides)
+  local overrides = window:get_config_overrides() or {}
+  overrides.enable_scroll_bar = not pane:is_alt_screen_active()
+  window:set_config_overrides(overrides)
 end)
 
 -- Finally, return the configuration to wezterm:
