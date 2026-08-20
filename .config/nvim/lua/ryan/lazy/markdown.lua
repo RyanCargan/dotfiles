bce3591e6f95b80513e04ddc2054ad01addc9d1e
Ryan <ryancargan@gmail.com>
2026-08-21 01:25:23 +0530

    Add Markdown wrap

--- .config/nvim/lua/ryan/lazy/markdown.lua
+++ .config/nvim/lua/ryan/lazy/markdown.lua
@@ -6,4 +6,15 @@ return {
     },
     ft = { "markdown" },
     opts = {},
+    config = function(_, opts)
+        require("render-markdown").setup(opts)
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "markdown",
+            callback = function()
+                vim.opt_local.wrap = true
+                vim.opt_local.linebreak = true
+                vim.opt_local.breakindent = true
+            end,
+        })
+    end,
 }
